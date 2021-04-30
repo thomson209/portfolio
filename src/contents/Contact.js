81870e8c730e4ea57b1dc5e6cf3b997c0ef08542
Your Name <you@example.com>
2021-04-30 14:21:55 -0230

    Minor refinement

--- src/contents/Contact.js
+++ src/contents/Contact.js
@@ -6,7 +6,7 @@ class Contact extends Component {
             <h1 className="subtopic">Contact Me</h1><br/>
             <h3>Douglas W. Thomson</h3>
             <h3>Port au Port, NL</h3><br/>
-            <h3>Cell  :  (709)721-0822</h3><br/>
+            <h3>Cell  :   (709)721-0822</h3><br/>
             <h3>Email  :   thomson209er@hotmail.com</h3>
             
             </div>
