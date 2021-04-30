394966de31ce07d29d76a6a9f21e63ce3f59528c
Your Name <you@example.com>
2021-04-30 14:20:37 -0230

    Added contact info

--- src/contents/Contact.js
+++ src/contents/Contact.js
@@ -4,6 +4,9 @@ class Contact extends Component {
         return (
             <div className="condiv">
             <h1 className="subtopic">Contact Me</h1><br/>
+            <h3>Douglas W. Thomson</h3>
+            <h3>Port au Port, NL</h3><br/>
+            <h3>Cell  :  (709)721-0822</h3><br/>
             <h3>Email  :   thomson209er@hotmail.com</h3>
             
             </div>
